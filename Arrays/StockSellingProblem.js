e627e46d8f66728a957742ede5844c39e1d7ff00
Sarthak Kumar Patro <patrosarthak3@gmail.com>
2025-04-18 21:51:10 +0530

    Wrong attempt for finding max profit for buy and sell stock instead wrote code for max and min difference. Correct approach in next commit

--- Arrays/StockSellingProblem.js
+++ Arrays/StockSellingProblem.js
@@ -1,13 +1,26 @@
-// Find maximum profit you could have made by selling the stock
-// 1. Method using sorting (not optimised)
-const arr = [7, 5, 6, 3, 4];
-arr.sort();
-console.log(arr);
-const len = arr.length - 1;
-const maxProfit = arr[len] - arr[0];
-console.log("Max Profit Possible: ", maxProfit);
+// // Find maximum profit you could have made by selling the stock
+// // 1. Method using sorting (not optimised)
+// // const arr = [7, 5, 6, 3, 4];
+// // arr.sort();
+// // const len = arr.length - 1;
+// // const maxProfit = arr[len] - arr[0];
+// // console.log("Max Profit Possible: ", maxProfit);
 
-// -----------------------------------------------------------------------------------------------------------------------
-// 2. Method using two pointers (optimised)
-// const arr = [7, 5, 6, 3, 4];
+// // -----------------------------------------------------------------------------------------------------------------------
+// // 2. Max And Min calculate
+// const arr = [7, 5, -6, 300, 40];
 // const len = arr.length - 1;
+// let min = 99999999;
+// let max = -99999999;
+// for (let index = 0; index < arr.length - 1; index++) {
+//   if (arr[index] > max) {
+//     max = arr[index];
+//   } else if (arr[index] < min) {
+//     min = arr[index];
+//   }
+// }
+
+// const maxProfit = max - min;
+
+// console.log(maxProfit);
+// -----------------------------------------------------------------------------------------------------------------------
